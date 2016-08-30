9096b578045e85a6c630f94d63a6bc4be5885ff7
pratulyak <pratulya.k@gmail.com>
2016-08-30 18:44:50 +1000

    first commit

---
+++ WebContent/frontpage.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@ page import="edu.unsw.comp9321.*"%>
+<jsp:useBean id="cart" class="edu.unsw.comp9321.Frontpage"
+	scope="session" />
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Digital Bibliography</title>
+</head>
+<body>
+<H1>This is working now</H1>
+<%@ include file="searchBar.html"%>
+</body>
+</html>
---
+++ src/edu/unsw/comp9321/Book.java
@@ -0,0 +1,80 @@
+package edu.unsw.comp9321;
+
+import java.util.List;
+
+public class Book {
+	private List<String> Authors;
+	private String Title;
+	private String publisher;
+	private int year;
+	private long pages;
+	private long isbn;
+	private String ee;
+	
+	public Book(String title,String publisher,int year,long pages,long isbn,String ee){
+		this.Title = title;
+		this.publisher = publisher;
+		this.year = year;
+		this.pages = pages;
+		this.isbn = isbn;
+		this.ee = ee;
+	}
+
+	public List<String> getAuthors() {
+		return Authors;
+	}
+
+	public void setAuthors(List<String> authors) {
+		Authors = authors;
+	}
+
+	public String getTitle() {
+		return Title;
+	}
+
+	public void setTitle(String title) {
+		Title = title;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public long getPages() {
+		return pages;
+	}
+
+	public void setPages(long pages) {
+		this.pages = pages;
+	}
+
+	public long getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(long isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getEe() {
+		return ee;
+	}
+
+	public void setEe(String ee) {
+		this.ee = ee;
+	}
+	
+	
+}
